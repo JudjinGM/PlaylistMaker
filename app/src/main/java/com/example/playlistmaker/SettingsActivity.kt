907b300ceca9e0a4bb2dd6fd53dfe4b9d27cd7e0
JudjinGM <judjingm@gmail.com>
2023-02-13 13:56:37 +0300

    Implement 2-nd homework

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +15,31 @@ class SettingsActivity : AppCompatActivity() {
         buttonBack.setOnClickListener {
             finish()
         }
+        val buttonShare = findViewById<ImageView>(R.id.image_share)
+        buttonShare.setOnClickListener {
+            val shareText = "https://practicum.yandex.ru/android-developer/"
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareText)
+            startActivity(Intent.createChooser(shareIntent, "Share" ))
+        }
+        val buttonSupport = findViewById<ImageView>(R.id.image_support)
+        buttonSupport.setOnClickListener {
+            val title = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val writeToSupportIntent = Intent(Intent.ACTION_SENDTO)
+            writeToSupportIntent.data = Uri.parse("mailto:")
+            writeToSupportIntent.putExtra(Intent.EXTRA_EMAIL,arrayOf("v4lievil@yandex.ru") )
+            writeToSupportIntent.putExtra(Intent.EXTRA_SUBJECT, title )
+            writeToSupportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(writeToSupportIntent)
+        }
+        val buttonAgreement = findViewById<ImageView>(R.id.image_term)
+        buttonAgreement.setOnClickListener {
+            val link = "https://yandex.ru/legal/practicum_offer/"
+            val agreementIntent = Intent(Intent.ACTION_VIEW)
+            agreementIntent.data = Uri.parse(link)
+            startActivity(agreementIntent)
+        }
     }
-
 }
