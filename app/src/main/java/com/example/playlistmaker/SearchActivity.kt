09ff973a90c65ed5419d9244e6a5ff7a736f10d1
JudjinGM <judjingm@gmail.com>
2023-02-15 11:28:33 +0300

    Sprint 9,Homework #2

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -11,32 +11,49 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val SAVED_TEXT = "SAVED_TEXT"
+    }
+
+    var inputSearchText: String? = null
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SAVED_TEXT, inputSearchText)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputSearchText = savedInstanceState.getString(SAVED_TEXT, "")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val inputSearchField = findViewById<EditText>(R.id.searchEditText)
-        val clearButton = findViewById<ImageView>(R.id.searchInputCloseIcon)
+        inputSearchField.setText(inputSearchText)
 
         inputSearchField.requestFocus()
 
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.showSoftInput(inputSearchField, InputMethodManager.SHOW_IMPLICIT)
-
+        val clearButton = findViewById<ImageView>(R.id.searchInputCloseIcon)
         clearButton.setOnClickListener {
             inputSearchField.setText("")
 
-            val inputMethodManagerClearButton = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            val inputMethodManagerClearButton =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManagerClearButton?.hideSoftInputFromWindow(inputSearchField.windowToken, 0)
         }
 
         val textWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
+                // empty
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 clearButton.visibility = clearButtonVisibility(s)
+                inputSearchText = inputSearchField.text.toString()
             }
 
             override fun afterTextChanged(s: Editable?) {
