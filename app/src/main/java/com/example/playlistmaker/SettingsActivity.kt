7a3468b6b1253333e51a558f66b966fabcfa63d7
JudjinGM <judjingm@gmail.com>
2023-02-19 18:07:25 +0300

    Sprint 9,Homework #2. Refactoring 3

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     private val intentEmailType = "text/plain"
-    val chooserTitle = "Share"
+    private val chooserTitle = "Share"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
