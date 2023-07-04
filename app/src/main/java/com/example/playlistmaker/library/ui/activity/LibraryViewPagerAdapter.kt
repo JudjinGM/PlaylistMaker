76ca5550be1bf48ae4e2442c15684f0799766824
JudjinGM <judjingm@gmail.com>
2023-07-04 14:22:26 +0400

    Sprint 18. Refactoring №1. Fixed issue with viewpager swipe and added small improvement based on reviewer suggestions.

--- app/src/main/java/com/example/playlistmaker/library/ui/activity/LibraryViewPagerAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/library/ui/activity/LibraryViewPagerAdapter.kt
@@ -5,13 +5,21 @@ import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.playlistmaker.library.ui.activity.fragments.FavoritesFragment
 import com.example.playlistmaker.library.ui.activity.fragments.PlaylistsFragment
+import com.example.playlistmaker.library.ui.activity.model.MediaFragmentType
 
 class LibraryViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int {
-        return 2
+        return NUMBER_OF_PAGES
     }
 
     override fun createFragment(position: Int) =
-        if (position == 0) FavoritesFragment.newInstance() else PlaylistsFragment.newInstance()
+        when (MediaFragmentType.values()[position]) {
+            MediaFragmentType.FAVORITES -> FavoritesFragment.newInstance()
+            MediaFragmentType.PLAYLIST -> PlaylistsFragment.newInstance()
+        }
+
+    companion object {
+        const val NUMBER_OF_PAGES = 2
+    }
 }
---
+++ app/src/main/java/com/example/playlistmaker/library/ui/activity/model/MediaFragmentType.kt
@@ -0,0 +1,6 @@
+package com.example.playlistmaker.library.ui.activity.model
+
+enum class MediaFragmentType {
+    FAVORITES,
+    PLAYLIST
+}
